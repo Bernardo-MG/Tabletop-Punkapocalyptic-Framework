55133c1b6147c0ed964610c60c48aaec7c9c8f64
Bernardo Martínez Garrido <programming@wandrell.com>
2014-08-29 22:30:27 +0200

    Added support to localize attributes.

--- src/main/java/com/wandrell/tabletop/punkapocalyptic/framework/conf/PunkapocalypticBundleConf.java
+++ src/main/java/com/wandrell/tabletop/punkapocalyptic/framework/conf/PunkapocalypticBundleConf.java
@@ -2,7 +2,8 @@ package com.wandrell.tabletop.punkapocalyptic.framework.conf;
 
 public final class PunkapocalypticBundleConf {
 
-    public static final String UNIT_NAME = "localization/UnitNameBundle";
+    public static final String UNIT_ATTRIBUTE = "localization/UnitAttributeBundle";
+    public static final String UNIT_NAME      = "localization/UnitNameBundle";
 
     private PunkapocalypticBundleConf() {
         super();
