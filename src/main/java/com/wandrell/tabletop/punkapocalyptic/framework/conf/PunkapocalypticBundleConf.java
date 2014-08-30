408015a402e6dd6c748305dfbffa7aa32a7c991b
Bernardo Martínez Garrido <programming@wandrell.com>
2014-08-30 20:10:50 +0200

    Added support for localizing armor names.

--- src/main/java/com/wandrell/tabletop/punkapocalyptic/framework/conf/PunkapocalypticBundleConf.java
+++ src/main/java/com/wandrell/tabletop/punkapocalyptic/framework/conf/PunkapocalypticBundleConf.java
@@ -2,6 +2,7 @@ package com.wandrell.tabletop.punkapocalyptic.framework.conf;
 
 public final class PunkapocalypticBundleConf {
 
+    public static final String ARMOR          = "localization/ArmorBundle";
     public static final String UNIT_ATTRIBUTE = "localization/UnitAttributeBundle";
     public static final String UNIT_NAME      = "localization/UnitNameBundle";
 
