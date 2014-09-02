4c55baa622462d0f7b113c00e20f26defefcfa60
Bernardo Martínez Garrido <programming@wandrell.com>
2014-09-03 01:50:14 +0200

    Added support for localizing weapons.

--- src/main/java/com/wandrell/tabletop/punkapocalyptic/framework/conf/PunkapocalypticBundleConf.java
+++ src/main/java/com/wandrell/tabletop/punkapocalyptic/framework/conf/PunkapocalypticBundleConf.java
@@ -5,6 +5,7 @@ public final class PunkapocalypticBundleConf {
     public static final String ARMOR          = "localization/ArmorBundle";
     public static final String UNIT_ATTRIBUTE = "localization/UnitAttributeBundle";
     public static final String UNIT_NAME      = "localization/UnitNameBundle";
+    public static final String WEAPON         = "localization/WeaponBundle";
 
     private PunkapocalypticBundleConf() {
         super();
