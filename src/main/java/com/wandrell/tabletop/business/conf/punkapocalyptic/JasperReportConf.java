7761770951a09b97b78e0c84fe555b3e9a7dbb11
Bernardo Martínez Garrido <programming@wandrell.com>
2014-09-28 19:03:47 +0200

    The gang report now shows correctly ranged weapons and armor.

--- src/main/java/com/wandrell/tabletop/business/conf/punkapocalyptic/JasperReportConf.java
+++ src/main/java/com/wandrell/tabletop/business/conf/punkapocalyptic/JasperReportConf.java
@@ -2,6 +2,7 @@ package com.wandrell.tabletop.business.conf.punkapocalyptic;
 
 public final class JasperReportConf {
 
+    public final static String PARAMETER_ARMORS_REPORT  = "armorsReport";
     public final static String PARAMETER_UNITS_REPORT   = "unitsReport";
     public final static String PARAMETER_WEAPONS_REPORT = "weaponsReport";
 
