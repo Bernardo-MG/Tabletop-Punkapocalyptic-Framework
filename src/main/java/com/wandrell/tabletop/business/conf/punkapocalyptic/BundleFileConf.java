12f9c7bb1e658589714c559e4cd1870d49ba6e2e
Bernardo Martínez Garrido <programming@wandrell.com>
2014-11-03 11:27:30 +0100

    Removed unused localization methods.

--- src/main/java/com/wandrell/tabletop/business/conf/punkapocalyptic/BundleFileConf.java
+++ src/main/java/com/wandrell/tabletop/business/conf/punkapocalyptic/BundleFileConf.java
@@ -2,12 +2,10 @@ package com.wandrell.tabletop.business.conf.punkapocalyptic;
 
 public final class BundleFileConf {
 
-    public static final String ARMOR            = "localization/ArmorBundle";
-    public static final String SPECIAL_RULE     = "localization/SpecialRuleBundle";
-    public static final String UNIT_ATTRIBUTE   = "localization/UnitAttributeBundle";
-    public static final String UNIT_NAME        = "localization/UnitNameBundle";
-    public static final String WEAPON           = "localization/WeaponBundle";
-    public static final String WEAPON_ATTRIBUTE = "localization/WeaponAttributeBundle";
+    public static final String ARMOR        = "localization/ArmorBundle";
+    public static final String SPECIAL_RULE = "localization/SpecialRuleBundle";
+    public static final String UNIT_NAME    = "localization/UnitNameBundle";
+    public static final String WEAPON       = "localization/WeaponBundle";
 
     private BundleFileConf() {
         super();
