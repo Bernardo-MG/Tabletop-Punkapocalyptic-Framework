4634a6215b9040594a7c7fa90f71c197033a02f4
Bernardo Martínez Garrido <programming@wandrell.com>
2014-11-24 23:55:00 +0100

    The gangs report now shows more data.

--- src/main/java/com/wandrell/tabletop/business/conf/punkapocalyptic/JasperReportConf.java
+++ src/main/java/com/wandrell/tabletop/business/conf/punkapocalyptic/JasperReportConf.java
@@ -5,6 +5,8 @@ public final class JasperReportConf {
     public final static String ARMORS_REPORT  = "armorsReport";
     public final static String UNITS_REPORT   = "unitsReport";
     public final static String WEAPONS_REPORT = "weaponsReport";
+    public final static String EQUIPMENT_REPORT = "equipmentReport";
+    public final static String ENHANCEMENTS_REPORT = "enhancementsReport";
 
     private JasperReportConf() {
         super();
