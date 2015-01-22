aa3ae666aa78b2d465f598158fb6a57573e43ede
Bernardo Martínez Garrido <programming@wandrell.com>
2015-01-22 23:23:22 +0100

    Added tests for the DefaultGangBuilderManager.

---
+++ src/test/java/com/wandrell/tabletop/testing/punkapocalyptic/framework/test/unit/procedure/TestDefaultGangBuilderManager.java
@@ -0,0 +1,204 @@
+package com.wandrell.tabletop.testing.punkapocalyptic.framework.test.unit.procedure;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import javax.sql.rowset.Predicate;
+
+import org.mockito.ArgumentCaptor;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.wandrell.tabletop.business.model.punkapocalyptic.availability.FactionUnitAvailability;
+import com.wandrell.tabletop.business.model.punkapocalyptic.unit.Gang;
+import com.wandrell.tabletop.business.model.punkapocalyptic.unit.Unit;
+import com.wandrell.tabletop.business.model.punkapocalyptic.unit.event.GangListener;
+import com.wandrell.tabletop.business.model.punkapocalyptic.unit.event.UnitEvent;
+import com.wandrell.tabletop.business.model.valuebox.ValueBox;
+import com.wandrell.tabletop.business.procedure.Constraint;
+import com.wandrell.tabletop.business.procedure.ConstraintValidator;
+import com.wandrell.tabletop.business.procedure.punkapocalyptic.DefaultGangBuilderManager;
+import com.wandrell.tabletop.business.service.punkapocalyptic.RulesetService;
+import com.wandrell.util.repository.Repository;
+
+public final class TestDefaultGangBuilderManager {
+
+    private DefaultGangBuilderManager           manager;
+    private Repository<FactionUnitAvailability> unitAvaRepository;
+    private ConstraintValidator                 validator;
+
+    public TestDefaultGangBuilderManager() {
+        super();
+    }
+
+    @SuppressWarnings("unchecked")
+    @BeforeMethod
+    public final void initialize() {
+        final Constraint unitLimitConstraint;
+        final ValueBox maxUnits;
+        final RulesetService rulesetService;
+
+        unitLimitConstraint = Mockito.mock(Constraint.class);
+        Mockito.when(unitLimitConstraint.toString()).thenReturn(
+                "UnitLimitConstraint");
+
+        validator = Mockito.mock(ConstraintValidator.class);
+        maxUnits = Mockito.mock(ValueBox.class);
+        unitAvaRepository = Mockito.mock(Repository.class);
+        rulesetService = Mockito.mock(RulesetService.class);
+
+        manager = new DefaultGangBuilderManager(unitLimitConstraint, validator,
+                maxUnits, unitAvaRepository, rulesetService);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public final void testAddUnit_UpdatesConstraints() {
+        final Gang gang;
+        final Unit unit1;
+        final Collection<Unit> units;
+        final ArgumentCaptor<GangListener> listenerCaptor;
+        final ArgumentCaptor<Constraint> constraintCaptor;
+        final GangListener listenerGang;
+        final Collection<FactionUnitAvailability> availabilities;
+        final FactionUnitAvailability ava;
+        final Collection<Constraint> constraints;
+        final Constraint constraint;
+        final Iterator<Constraint> itrConstraints;
+
+        listenerCaptor = ArgumentCaptor.forClass(GangListener.class);
+        constraintCaptor = ArgumentCaptor.forClass(Constraint.class);
+
+        constraints = new LinkedList<>();
+
+        constraint = Mockito.mock(Constraint.class);
+        Mockito.when(constraint.isValid()).thenReturn(false);
+        Mockito.when(constraint.toString()).thenReturn("MockedConstraint1");
+
+        constraints.add(constraint);
+
+        ava = Mockito.mock(FactionUnitAvailability.class);
+
+        Mockito.when(ava.getConstraints()).thenReturn(constraints);
+
+        availabilities = new LinkedList<>();
+        availabilities.add(ava);
+
+        Mockito.when(
+                unitAvaRepository
+                        .getCollection((java.util.function.Predicate<FactionUnitAvailability>) Matchers
+                                .any(Predicate.class))).thenReturn(
+                availabilities);
+
+        gang = Mockito.mock(Gang.class);
+
+        unit1 = Mockito.mock(Unit.class);
+
+        units = new LinkedList<>();
+
+        Mockito.when(gang.getUnits()).thenReturn(units);
+
+        manager.setGang(gang);
+
+        Mockito.verify(gang, Mockito.times(1)).addGangListener(
+                listenerCaptor.capture());
+
+        listenerGang = listenerCaptor.getValue();
+
+        listenerGang.unitAdded(new UnitEvent(this, unit1));
+
+        Mockito.verify(validator, Mockito.atLeastOnce()).addConstraint(
+                constraintCaptor.capture());
+
+        Assert.assertEquals(constraintCaptor.getAllValues().size(), 2);
+
+        itrConstraints = constraintCaptor.getAllValues().iterator();
+        itrConstraints.next();
+        Assert.assertEquals(itrConstraints.next(), constraint);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public final void testRemoveUnit_UpdatesConstraints() {
+        final Gang gang;
+        final Unit unit1;
+        final Collection<Unit> units;
+        final ArgumentCaptor<GangListener> listenerCaptor;
+        final ArgumentCaptor<Constraint> constraintCaptor;
+        final GangListener listenerGang;
+        final Collection<FactionUnitAvailability> availabilities;
+        final FactionUnitAvailability ava;
+        final Collection<Constraint> constraints;
+        final Constraint constraint;
+        final Iterator<Constraint> itrConstraints;
+
+        listenerCaptor = ArgumentCaptor.forClass(GangListener.class);
+        constraintCaptor = ArgumentCaptor.forClass(Constraint.class);
+
+        constraints = new LinkedList<>();
+
+        constraint = Mockito.mock(Constraint.class);
+        Mockito.when(constraint.isValid()).thenReturn(false);
+        Mockito.when(constraint.toString()).thenReturn("MockedConstraint1");
+
+        constraints.add(constraint);
+
+        ava = Mockito.mock(FactionUnitAvailability.class);
+
+        Mockito.when(ava.getConstraints()).thenReturn(constraints);
+
+        availabilities = new LinkedList<>();
+        availabilities.add(ava);
+
+        Mockito.when(
+                unitAvaRepository
+                        .getCollection((java.util.function.Predicate<FactionUnitAvailability>) Matchers
+                                .any(Predicate.class))).thenReturn(
+                availabilities);
+
+        gang = Mockito.mock(Gang.class);
+
+        unit1 = Mockito.mock(Unit.class);
+
+        units = new LinkedList<>();
+        units.add(unit1);
+
+        Mockito.when(gang.getUnits()).thenReturn(units);
+
+        manager.setGang(gang);
+
+        Mockito.verify(gang, Mockito.times(1)).addGangListener(
+                listenerCaptor.capture());
+
+        listenerGang = listenerCaptor.getValue();
+
+        listenerGang.unitRemoved(new UnitEvent(this, unit1));
+
+        Mockito.verify(validator, Mockito.atLeastOnce()).addConstraint(
+                constraintCaptor.capture());
+
+        // Assert.assertEquals(constraintCaptor.getAllValues().size(), 2);
+
+        itrConstraints = constraintCaptor.getAllValues().iterator();
+        itrConstraints.next();
+        itrConstraints.next();
+        Assert.assertEquals(itrConstraints.next(), constraint);
+    }
+
+    @Test
+    public final void testValidate_NoValidates() {
+        Mockito.when(validator.validate()).thenReturn(false);
+        Assert.assertTrue(!manager.validate());
+    }
+
+    @Test
+    public final void testValidate_Validates() {
+        Mockito.when(validator.validate()).thenReturn(true);
+        Assert.assertTrue(manager.validate());
+    }
+
+}
