973c19f333802de8d3fa210bf3e9df14ee74b76e
Bernardo Martínez Garrido <programming@wandrell.com>
2015-06-06 21:52:41 +0200

    Corrected factories.

--- src/test/java/com/wandrell/tabletop/testing/punkapocalyptic/framework/framework/conf/factory/parameter/ConstraintParameterFactory.java
+++ src/test/java/com/wandrell/tabletop/testing/punkapocalyptic/framework/framework/conf/factory/parameter/ConstraintParameterFactory.java
@@ -1,5 +1,15 @@
 package com.wandrell.tabletop.testing.punkapocalyptic.framework.framework.conf.factory.parameter;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
@@ -31,7 +41,6 @@ import com.wandrell.tabletop.testing.punkapocalyptic.framework.framework.util.pa
 import com.wandrell.tabletop.testing.punkapocalyptic.framework.framework.util.parser.UpToCountDocumentParser;
 import com.wandrell.tabletop.testing.punkapocalyptic.framework.framework.util.parser.UpToHalfDocumentParser;
 import com.wandrell.tabletop.testing.punkapocalyptic.framework.framework.util.parser.WeaponIntervalDocumentParser;
-import com.wandrell.util.ResourceUtils;
 
 public final class ConstraintParameterFactory {
 
@@ -117,6 +126,65 @@ public final class ConstraintParameterFactory {
         return getWeaponIntervalConstraintParameters(getValues(
                 ConstraintParametersConf.WEAPON_INTERVAL, true,
                 new WeaponIntervalDocumentParser()));
+    }
+
+    /**
+     * Creates an {@code InputStream} pointing to the file specified by the
+     * path, if it exists.
+     * <p>
+     * If any problem occurs during this process a {@code RuntimeException} is
+     * thrown, or an {@code IllegalArgumentException} if the file is not found.
+     * 
+     * @param path
+     *            the path to transform
+     * @return an {@code InputStream} pointing to the path
+     */
+    private final InputStream getClassPathInputStream(final String path) {
+        final URL url;                  // URL parsed from the path
+        final InputStream stream;       // Stream for the file
+
+        url = getClassPathURL(path);
+
+        checkArgument(url != null,
+                String.format("The path %s is invalid", path));
+
+        try {
+            stream = new BufferedInputStream(url.openStream());
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return stream;
+    }
+
+    /**
+     * Creates a {@code Reader} pointing to the file specified by the path, if
+     * it exists.
+     * <p>
+     * If any problem occurs during this process a {@code RuntimeException} is
+     * thrown, or an {@code IllegalArgumentException} if the file is not found.
+     * 
+     * @param path
+     *            the path to transform
+     * @return an {@code InputStream} pointing to the path
+     */
+    private final Reader getClassPathReader(final String path) {
+        return new BufferedReader(new InputStreamReader(
+                getClassPathInputStream(path)));
+    }
+
+    /**
+     * Creates an {@code URL} pointing to the file specified by the path, if it
+     * exists.
+     * 
+     * @param path
+     *            the path to transform
+     * @return an URL pointing inside the class path
+     */
+    private final URL getClassPathURL(final String path) {
+        checkNotNull(path, "Received a null pointer as path");
+
+        return this.getClass().getClassLoader().getResource(path);
     }
 
     private final Iterator<Object[]> getDependantConstraintParameters(
@@ -342,8 +410,7 @@ public final class ConstraintParameterFactory {
             parserFile.addRejectedAttribute("valid");
         }
 
-        return parserParams.parse(parserFile.parse(ResourceUtils
-                .getClassPathReader(file)));
+        return parserParams.parse(parserFile.parse(getClassPathReader(file)));
     }
 
     @SuppressWarnings("unchecked")
